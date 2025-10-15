f78a277ecf41b66c7bbe2500990dff0a3948f012
felipesoares-bit <pedrosoaresdriver@gmail.com>
2025-10-15 17:12:21 -0300

    Update vite.config.js

--- lazertur-assistente-main/frontend/vite.config.js
+++ lazertur-assistente-main/frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '',
   build: {
     outDir: 'dist'
   },
@@ -11,4 +11,5 @@ export default defineConfig({
     port: 5173
   }
 })
+
 
